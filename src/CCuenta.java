ad98231e3942861b5fd1bd6c3e796340dca86f93
TATIANA <tatgaramo@alu.edu.gva.es>
2023-02-21 21:53:04 +0100

    getCuenta y Tipo de Interés

--- src/CCuenta.java
+++ src/CCuenta.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class CCuenta {
+public class CCuenta {
+
+    // Atributos de cuenta
+    private String nombre;
+    private String cuenta;
+    private double saldo;
+    private double tipoInteres;
+
+    public CCuenta(String nom, String cue, double sal, double tipo) {
+        nombre = nom;
+        cuenta = cue;
+        saldo = sal;
+        tipoInteres = tipo;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    // Para probar Junit
+    public void ingresar(double cantidad) {
+        if (cantidad < 0)
+            System.out.println("Cantidad negativa");
+        saldo = saldo + cantidad;
+    }
+
+    // Para probar JUnit
+    public void retirar(double cantidad) {
+        if (cantidad < 0)
+            System.out.println("Cantidad Negativa");
+        if (this.getSaldo() < cantidad)
+            System.out.println("Saldo insuficiente");
+        saldo = saldo - cantidad;
+    }
+
+    // Método que me devuelve el número de cuenta
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
 }
